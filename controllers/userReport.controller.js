25587b41d67fcd1de137aa425742bdaf1ddb4e30
amanasthana1111 <as.asthana.1111@gmail.com>
2026-01-07 14:30:48 +0530

    remove log and send res

--- controllers/userReport.controller.js
+++ controllers/userReport.controller.js
@@ -4,8 +4,5 @@ import { reportAi } from "../utils/googleGemini2.js"
  export const Report = async (req,res )=>{
     const userObj = req.body;
     const responseData = await reportAi(userReportConfig,userObj);
-    console.log(responseData)
-    return res.json({
-        mess : "done"
-    })
+    return res.status(200).json(responseData)
 }
